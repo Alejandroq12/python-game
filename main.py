78dc347ba18914b6984500dabfe5a91c98e01bf3
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-10-31 13:13:13 -0600

    Refactor: Add asteroids.

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -20,16 +31,15 @@ def main():
         for player_element in updatable:
             player_element.update(dt)
 
-            screen.fill("black")
+        screen.fill("black")
 
         for player_element in drawable:
             player_element.draw(screen)   
-        
 
-            pygame.display.flip()
+        pygame.display.flip()
             
             # limit the framerate to 60 fps
-            dt = clock.tick(60) / 1000
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
