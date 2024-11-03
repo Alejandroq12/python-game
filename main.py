187176f8bfd31bd61f97de5710f2c12d517fca11
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-11-02 22:14:50 -0600

    Refactor: Prepare to limit the framerate to 60 FPS.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -12,6 +14,9 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+        
+        # limit the framerate to 60 fps
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
