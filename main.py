0b140f5e90d5934007b1d0ad839807f00c775afb
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-11-04 15:39:03 -0600

    Refactor: Draw player.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -11,8 +13,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+            
         screen.fill("black")
+        player.draw(screen)    
         pygame.display.flip()
         
         # limit the framerate to 60 fps
