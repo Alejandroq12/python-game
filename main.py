b6b78787f289856ae12b7243fd8ad948df459cc0
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-11-05 14:40:11 -0600

    Refactor: Add groups.

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from player import Player
 def main():
     pygame.init()
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -13,14 +16,20 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        for player_element in updatable:
+            player_element.update(dt)
+
+            screen.fill("black")
+
+        for player_element in drawable:
+            player_element.draw(screen)   
         
-        player.update(dt)
-        screen.fill("black")
-        player.draw(screen)    
-        pygame.display.flip()
-        
-        # limit the framerate to 60 fps
-        dt = clock.tick(60) / 1000
+
+            pygame.display.flip()
+            
+            # limit the framerate to 60 fps
+            dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
