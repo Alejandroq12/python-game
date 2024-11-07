65c7e76bd3f1d981951f895d8d473e2275a15dd2
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-11-06 20:30:30 -0600

    Feat: Add shoot timer.

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def main():
 
         pygame.display.flip()
             
-            # limit the framerate to 60 fps
+        # limit the framerate to 60 fps
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
